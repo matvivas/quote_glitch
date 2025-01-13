03b7890f4c1e2ef6160ae08eb6f168d876b1abc9
matvivas <mateusavivas@gmail.com>
2025-01-13 17:24:55 -0300

    ultima att

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def get_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    options.add_argument("--headless") # Sem interface gráfica
     options.add_experimental_option("detach", True)  # Mantém o navegador aberto
     options.add_argument("--start-maximized")        # Abre o navegador em tela cheia
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
